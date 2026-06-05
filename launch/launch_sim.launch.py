a96ab54eaea1e79601755c03450cf94d9d4ca6c9
Rana Usama Naeem <usamanaeem803@gmail.com>
2026-06-05 21:33:12 +0500

    changes related to Joystick and Navigation

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,7 +2,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -11,27 +10,22 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='fyp' #<--- CHANGE ME
+    package_name='fyp'
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
-    
+
     default_world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
         'empty.world'
-        )    
-    
+        )
+
     world = LaunchConfiguration('world')
 
     world_arg = DeclareLaunchArgument(
@@ -40,22 +34,18 @@ def generate_launch_description():
         description='World to load'
         )
 
-    # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
                     launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', 'fyp',
                                    '-z', '0.1'],
                         output='screen')
 
-
-    # Launch the ROS-Gazebo bridge for normal topics
     bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
@@ -67,13 +57,53 @@ def generate_launch_description():
         ]
     )
 
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diff_cont",
+            '--controller-ros-args',
+            '-r /diff_cont/cmd_vel:=/cmd_vel'
+        ],
+    )
 
-    # Launch them all!
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    joystick = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'joystick.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        parameters=[
+            os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml'),
+            {'use_sim_time': True}
+        ],
+        remappings=[('/cmd_vel_out', '/cmd_vel')]
+    )
+
     return LaunchDescription([
         rsp,
         world_arg,
         gazebo,
         spawn_entity,
         ros_gz_bridge,
+        ros_gz_image_bridge,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        joystick,
+        twist_mux,
     ])
